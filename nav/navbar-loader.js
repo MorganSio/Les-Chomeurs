92beac932155b8b5097d85451ee8bfa5b0a514b1
MorganSio <161039598+MorganSio@users.noreply.github.com>
2025-12-05 06:18:27 +0100

    nav

--- nav/navbar-loader.js
+++
@@ -1,7 +0,0 @@
-// Charger le fichier header.html et l'insérer dans la page
-fetch('/nav/navbar.html')
-.then(response => response.text())
-.then(data => {
-  document.getElementById('header').innerHTML = data;
-})
-.catch(error => console.error('Erreur lors du chargement du header:', error));
